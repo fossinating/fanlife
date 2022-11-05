5ec2ffc3321ca0ae34b8c8948c52bbfa4c83d52c
Gabrian Chua <57601516+gabrianrchua@users.noreply.github.com>
2022-11-05 11:28:55 -0400

    add home and activity components

--- fanlife/src/app/app-routing.module.ts
+++ fanlife/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActivitiesComponent } from './activities/activities.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'activites', component: ActivitiesComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: '/' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- fanlife/src/app/app.module.ts
+++ fanlife/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivitiesComponent } from './activities/activities.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivitiesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
