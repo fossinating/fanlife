1b9e4e10df1be644797cad6080a7e3e86018e5b2
Nolan <nolanwelch@outlook.com>
2022-11-05 13:08:15 -0400

    Integrate firebase config

--- fanlife/src/main.ts
+++ fanlife/src/main.ts
@@ -1,6 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { initializeApp } from "firebase/app";
+
+import firebaseConfig from 'firebase-config.json' assert {type: 'json'};
+
+const app = initializeApp(firebaseConfig);
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
