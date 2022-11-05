0ae3d448b378367c862caabf4ba65813128da3a8
Nolan <nolanwelch@outlook.com>
2022-11-05 13:40:21 -0400

    Properly implement firebase

--- fanlife/src/environments/environment.ts
+++ fanlife/src/environments/environment.ts
@@ -1,1 +1,11 @@
-// Request actual environment.ts file to run.
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAA2RKFPOteWZYk97sPsvAK7A2a6ZxcQAk',
+    authDomain: 'fanlife-hacknc.firebaseapp.com',
+    projectId: 'fanlife-hacknc',
+    storageBucket: 'fanlife-hacknc.appspot.com',
+    messagingSenderId: '525467649947',
+    appId: '1:525467649947:web:887b38ba8561742fbe433e'
+  }
+};
