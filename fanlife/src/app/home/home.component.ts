97ed52134e5dd6b78123ecfe0e6088f9e35caa98
Gabrian Chua <57601516+gabrianrchua@users.noreply.github.com>
2022-11-05 16:39:38 -0400

    add next button function

--- fanlife/src/app/home/home.component.ts
+++ fanlife/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GameLogsService } from './gamelog/glogs.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private glogService: GameLogsService) { }
 
   ngOnInit(): void {
+  }
+
+  nextBtn(): void {
+    
   }
 
 }
